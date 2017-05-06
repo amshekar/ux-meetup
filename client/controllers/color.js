181c845c046522164ea98c46ad3f5a7136541e36
nithyanantham s <nithyananthamn@gmail.com>
2017-05-07 01:30:04 +0530

    color save

--- client/controllers/color.js
+++ client/controllers/color.js
@@ -17,8 +17,9 @@ angular.module('MyApp')
             
             $scope.style ={};
             $scope.style.colors = ['#f54337','#e91d62','#9c28b1','#673bb7','#3f51b5','#2196f3','#03a9f5','#00bbd4','#009788','#4cb050','#8bc24a','#cddc39','#ffeb3c','#fec107'];
-
+      
       $scope.updateProfile = function() {
+      $scope.user = {'font':[$scope.color1,$scope.color2]};
       Account.updateProfile($scope.user)
         .then(function() {
           toastr.success('Profile has been updated');
