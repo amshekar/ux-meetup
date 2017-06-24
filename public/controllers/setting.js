ebd0e62f3db319c50c6bf93c59f27fe255c2e6bd
Nithyanantham S <nithyananthamn@gmail.com>
2017-06-24 23:56:28 +0530

    font, color validation and font filter first char issue fx

--- public/controllers/setting.js
+++ public/controllers/setting.js
@@ -11,12 +11,20 @@ angular.module('MyApp')
     };
     $scope.cl1 = '';
     $scope.cl2 = '';
+    $scope.isFontError = false;
+    $scope.isColorError = false;
 
     $scope.setColor = function (){
       var color1Value = window.document.getElementById('search-color');
       var color2Value = window.document.getElementById('search-color1');
-      $scope.cl1 = color1Value.value;
-      $scope.cl2 = color2Value.value;
+      if(color1Value.value != '' && color2Value.value != '')      {
+        $scope.isColorError = false;
+        $scope.cl1 = color1Value.value;
+        $scope.cl2 = color2Value.value;
+        $state.go('setting.font');
+      }else{
+        $scope.isColorError = true;
+      }
     };
 
      $scope.filterSelected = true;
@@ -30,21 +38,27 @@ angular.module('MyApp')
        function createFilterFor(query) {
           var lowercaseQuery = angular.lowercase(query);
           return function filterFn(contact) {
-            return (contact.indexOf(lowercaseQuery) != -1);;
+            var lowerContact = angular.lowercase(contact);
+            return (lowerContact.indexOf(lowercaseQuery) != -1);
           };
       }
 
     $scope.updateProfile = function() {
-     
-      $scope.user = {'font':$scope.selectedItem.fonts, 'color':[$scope.cl1,$scope.cl2]};
-      Account.updateProfile($scope.user)
-        .then(function() {
-          //toastr.success('Profile has been updated');
-          $state.go('home');
-        })
-        .catch(function(response) {
-          toastr.error(response.data.message, response.status);
-        });
+      if($scope.selectedItem.fonts.length == 2)
+      {
+        $scope.isFontError = false;
+        $scope.user = {'font':$scope.selectedItem.fonts, 'color':[$scope.cl1,$scope.cl2]};
+        Account.updateProfile($scope.user)
+          .then(function() {
+            //toastr.success('Profile has been updated');
+            $state.go('home');
+          })
+          .catch(function(response) {
+            toastr.error(response.data.message, response.status);
+          });
+      }else{
+        $scope.isFontError = true;
+      }
    };
 
    $scope.init = function()
