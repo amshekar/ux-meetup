13984b0da5b8a4e07a7779192974e592192e25d5
amshekar <amshekar@hotmail.com>
2017-09-20 23:31:27 +0530

    error

---
+++ public/controllers/error.js
@@ -0,0 +1,17 @@
+/* --------------------------------------------------------------------------------------------------------------------
+<copyright file="server.js" company="Bytes2Bots">
+   (c) Copyright 2017
+ </copyright>
+ <summary>
+ 
+ </summary>
+ --------------------------------------------------------------------------------------------------------------------*/
+ (function (angular) {
+    function ErrorCtrl($scope, $http, $auth, toastr, Account) {
+       
+    }
+
+    ErrorCtrl.$inject = ["$scope", "$http", "$auth", "toastr", "Account"];
+    angular.module('MyApp').controller("ErrorCtrl", ErrorCtrl);
+
+})(angular);
