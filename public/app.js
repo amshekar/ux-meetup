651b4e92cc42e9c4a805303e0e209d5862acb212
amshekar <amshekar@hotmail.com>
2017-09-22 23:10:54 +0530

    test

--- public/app.js
+++ public/app.js
@@ -102,7 +102,7 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr',
             });
         $urlRouterProvider.otherwise('/');
         // use the HTML5 History API
-      // $locationProvider.html5Mode(true);
+       $locationProvider.html5Mode(true);
        
 
         /**
